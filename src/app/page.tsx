f5ff436541bda23eb3f5945c4e5f69eee7e22a22
Fer457 <fernandonevothernandez@gmail.com>
2023-11-18 22:17:00 +0100

    fix: removed chakra ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 "use client"
-import { Box, Button, FormControl, FormLabel, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useDisclosure } from '@chakra-ui/react'
 import { useRef, useState } from 'react'
 
 export default function Home() {
-  const { isOpen, onOpen, onClose } = useDisclosure()
 
   const initialRef = useRef(null)
   const finalRef = useRef(null)
@@ -13,52 +11,12 @@ export default function Home() {
 
   return (
     <>
-    <Modal
-        initialFocusRef={initialRef}
-        finalFocusRef={finalRef}
-        isOpen={isOpen}
-        onClose={onClose}
-        size={'xl'}
-      >
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>$nombre</ModalHeader>
-          <ModalCloseButton />
-          <ModalBody pb={6}>
-            <FormControl>
-               <textarea
-                    required
-                    className="w-full h-56 rounded-lg border-gray-200 p-3 text-sm "
-                    placeholder="¿Sobre qué quieres hablar?"
-                    id="message"></textarea>
-            </FormControl>
-            {showDatePicker && (
-              <Box mt={4}>
-                {/* <DateTimePicker
-                  onChange={onChange}
-                  value={value}
-                /> */}
-              </Box>
-            )}
-          </ModalBody>
-          <ModalFooter>
-            <Button mr={3} onClick={() => setShowDatePicker(!showDatePicker)} >
-              🕘
-            </Button>
-            <Button  mr={3} type='submit'>
-              Publicar
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
 <section>
   <div
     className="mx-auto max-w-screen-xl px-4 py-8 sm:py-12 sm:px-6 lg:py-16 lg:px-8"
   >
     <div className="mx-auto max-w-lg text-center">
       <h2 className="text-3xl font-bold sm:text-4xl">Bienvenido</h2>
-
-      <Button onClick={onOpen}>Publicar</Button>
     </div>
 
     <div className="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
