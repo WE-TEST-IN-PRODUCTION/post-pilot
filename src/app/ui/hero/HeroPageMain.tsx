4c0ba710dac2105302da800bd1022b4dfe26aa96
Robluchy <Dionny75@gmail.com>
2023-11-19 13:25:53 +0100

    cambios esteticos

--- src/app/ui/hero/HeroPageMain.tsx
+++ src/app/ui/hero/HeroPageMain.tsx
@@ -3,7 +3,7 @@ import HeroMainContent from "./HeroMainContent";
 
 const HeroPageMain: React.FC = () => {
   return (
-    <main className="flex flex-col py-36 px-[30%] text-center items-center space-y-8">
+    <main className="flex flex-col py-24 px-[30%] text-center items-center space-y-8">
       <HeroMainContent />
       <HeroMainButtons />
     </main>
